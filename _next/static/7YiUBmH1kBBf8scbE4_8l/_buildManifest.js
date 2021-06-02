0daf1c1df57f133ef03d7285f0b0a34f1a14cfe6
takeshisakuma <saude0518@gmail.com>
2021-06-02 23:07:05 +0900

    Updates

---
+++ _next/static/7YiUBmH1kBBf8scbE4_8l/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4dd076afe26787d482db.css","static/chunks/pages/index-0f2ab6abeb2ba6944a9d.js"],"/_error":["static/chunks/pages/_error-d799e3a96772416696b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
