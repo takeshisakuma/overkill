a258b058d72da3d77b37c2cb9adddbd4c4eb55b8
takeshisakuma <saude0518@gmail.com>
2021-06-02 21:23:05 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Main = () => (
       pagePath={"https://takeshisakuma.github.io/overkill/"}
     />
     <p className={styles.test}>next.js test6</p>
-    <Image src="/test.png" height={100} width={100} quality={50} alt="test" />
+    {/*<Image src="/test.png" height={100} width={100} quality={50} alt="test" />*/}
   </>
 );
 export default Main;
