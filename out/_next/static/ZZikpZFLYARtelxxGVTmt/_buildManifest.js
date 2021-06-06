c4675e43733062a16ab38877575f965cca9b07ed
takeshi sakuma <saude0518@gmail.com>
2021-06-06 23:46:13 +0900

    Delete out directory

--- out/_next/static/ZZikpZFLYARtelxxGVTmt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bf332b24054c44251a9b.css","static/chunks/pages/index-f6148e61ee7c68f49327.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
